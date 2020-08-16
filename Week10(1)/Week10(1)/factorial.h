f54d1116147c61129c75cad86a51606aadec6abf
Liarn7 <1349624273@qq.com>
2020-08-16 15:33:42 +0800

    x

--- Week10(1)/Week10(1)/factorial.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-int pchar(int n);
-class FAC {
-	public:
-	float convert(float deg);
-};
-
--- Week9/Win32Project1/win32.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include"win32.h"
-char* pchar()
-{
-	return "¾²Ì¬¿â";
-}
-int F1::GetA() {
-	return a + 1;
-}
